d0524b3b543cc846fa25a7e9f1dddf034b1c2452
Matthew Gertner <admin@salsitasoft.com>
2012-02-08 11:56:01 +0100

    First implementation of JSON schema-based validator

---
+++ xml2js-schema.js
@@ -0,0 +1,87 @@
+(function() {
+  exports.Validator = (function() {
+
+    function Validator(schema) {
+      this._schema = schema;
+    }
+
+    Validator.prototype.validate = function(obj, stack, nodeName) {
+      var s = stack[stack.length - 1];
+      var schemaNode;
+      var schemaNodeName;
+      var i, name;
+      var properties = null;
+      var nodeType;
+      for (i = 0; i < stack.length+1; i++) {
+        if (i < stack.length) {
+          name = stack[i]["#name"];
+        }
+        else {
+          name = nodeName;
+        }
+        if (i === 0) {
+          // We're at the bottom of the stack so get the root item from the schema.
+          // It is always an object.
+          schemaNode = this._schema;
+          schemaNodeName = schemaNode.name;
+          nodeType = "object";
+          if (name !== schemaNodeName) {
+            // Oops, we're not the kind of object we expected.
+            throw new Error("Expected object " + schemaNodeName + " but found " + name);
+          }
+        }
+        else {
+          // Otherwise we use the name of this item to get its schema info from the
+          // parent item's list of properties.
+          schemaNode = properties[name];
+          schemaNodeName = name;
+          nodeType = schemaNode.type;
+        }
+        // Remember our properties for the next item in the stack.
+        if (nodeType == "object") {
+          properties = schemaNode.properties;
+        }
+        else {
+          properties = null;
+        }
+        if (nodeType === "array") {
+          // We're an array so we want the processing to apply to items in the
+          // array.
+          nodeType = schemaNode.items.type;
+          properties = schemaNode.items.properties;
+          // If we're at the top of the stack and our value is not an array yet
+          // then make it into an array. That way it will be an array even if it ends
+          // up with less than two items in it.
+          if ((i === stack.length) && !(s[nodeName] instanceof Array)) {
+            s[nodeName] = [];
+          }
+        }
+      }
+      // Now we've walked up the whole stack so we have the node type of the last item.
+      if (nodeType !== "object") {
+        if (obj instanceof Object) {
+          throw new Error("Cannot assign an object to literal property " + name);
+        }
+        // Convert the datatype of the value if necessary.
+        if (nodeType === "number") {
+          obj = Number(obj);
+        }
+        // TODO: Could add more conversions here if we need them.
+      }
+      else {
+        if (obj instanceof Object) {
+          // Check that all required properties are present
+          for (propName in properties) {
+            if (properties[propName].required && !(propName in obj)) {
+              throw new Error("Object " + name + " is missing required property " + propName);
+            }
+          }
+        } else {
+          throw new Error("Cannot assign a literal to object property " + name);
+        }
+      }
+      return obj;
+    }
+    return Validator;
+  })();
+}).call(this);
